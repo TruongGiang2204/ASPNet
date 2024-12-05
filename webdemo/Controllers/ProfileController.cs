088e903b2e7a03f1cb88cfcf06defce930be12f6
TruongGiang2204 <nguyentruonggiangmc113@gmail.com>
2024-12-05 09:16:13 +0700

    Initial commit

---
+++ webdemo/Controllers/ProfileController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace webdemo.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AccountSetting()
+        {
+            return View();
+        }
+        public ActionResult Address()
+        {
+            return View();
+        }
+        public ActionResult Wishlist()
+        {
+            return View();
+        }
+        public ActionResult SellingItems()
+        {
+            return View();
+        }
+        public ActionResult Orders()
+        {
+            return View();
+        }
+    }
+}
